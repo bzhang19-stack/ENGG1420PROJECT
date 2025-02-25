404ccbb7f0cc0087a555ba44a92ea9fbfde72161
Eric <eric.sacher88@gmail.com>
2025-02-25 15:06:08 -0500

    testing

--- src/main/java/com/example/project/Testing.java
+++ src/main/java/com/example/project/Testing.java
@@ -2,8 +2,8 @@ package com.example.project;
 
 public class Testing {
 
-public class Main {
-    public static void main(String[] args) {
+static class main {
+    public static void Main(String[] args) {
         System.out.println("This is a testing");
     }
 }
