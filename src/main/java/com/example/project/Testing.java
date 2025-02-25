98710ca9180df6b64fa6e6f3f372e22063a304d6
Jack <mullinj@uoguelph.ca>
2025-02-25 15:06:58 -0500

    Test

--- src/main/java/com/example/project/Testing.java
+++ src/main/java/com/example/project/Testing.java
@@ -2,8 +2,8 @@ package com.example.project;
 
 public class Testing {
 
-static class main {
-    public static void Main(String[] args) {
+static class Main {
+    public static void main(String[] args) {
         System.out.println("This is a testing");
     }
 }
