04fc1de994d1c828727f4c8c8a4306fa7c90c5cc
JackM0606 <mullinj@uoguelph.ca>
2025-03-04 01:29:00 -0500

    added method to access admin menu fxml

--- src/main/java/sceneControllers/AdminMenuController.java
+++ src/main/java/sceneControllers/AdminMenuController.java
@@ -1,8 +1,10 @@
 package sceneControllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +12,16 @@ import java.util.ResourceBundle;
 public class AdminMenuController implements Initializable{
     @FXML
     private ChoiceBox<String> dropdownMenu;
+    private Label adminMenuLabel;
     private final String[] options = {"Dashboard","Subject Management","Course Management","Student Management","Faculty Management","Even Management"}; // Options in choice box
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         dropdownMenu.getItems().addAll(options); // Populates choice box with options
+    }
+
+    public void getSelection(ActionEvent event){
+        String selection = dropdownMenu.getValue();
+        adminMenuLabel.setText("You have selected: "+selection);
     }
 }
